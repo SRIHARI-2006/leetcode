019bb82a5fb6443a23df33a89be5e1d44123b0e8
SRIHARI-2006 <164837169+SRIHARI-2006@users.noreply.github.com>
2026-07-01 10:18:39 +0530

    Time: 2 ms (92.02%) | Memory: 47.7 MB (56.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,16 +3,18 @@ class Solution {
 
         int index = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
+        for (int i = 0; i<nums.length;i++){
+            if (nums[i] != 0){
                 nums[index] = nums[i];
                 index++;
             }
         }
-
-        while(index < nums.length){
-            nums[index] = 0;
-            index++;
+            while (index < nums.length){
+                nums[index] = 0;
+                index++;
+            
         }
     }
 }
+
+    
